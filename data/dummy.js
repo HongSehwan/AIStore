b9775d53394ca97347a60abe5d78ab9d4e8da3be
HongSehwan <gg9297@gmail.com>
2024-01-14 18:02:55 +0900

    더미 파일 생성

---
+++ data/dummy.js
@@ -0,0 +1,66 @@
+const storeItem = {
+    clothing: [
+        {
+            type: "top",
+            name: "top1",
+            size: "free",
+            img: "/images/clothing1.jpeg",
+        },
+        {
+            type: "top",
+            name: "top2",
+            size: "free",
+            img: "/images/clothing2.jpeg",
+        },
+        {
+            type: "top",
+            name: "top3",
+            size: "free",
+            img: "/images/clothing3.jpeg",
+        },
+        {
+            type: "top",
+            name: "top4",
+            size: "free",
+            img: "/images/clothing4.jpeg",
+        },
+        {
+            type: "top",
+            name: "top5",
+            size: "free",
+            img: "/images/clothing5.jpeg",
+        },
+        {
+            type: "top",
+            name: "top6",
+            size: "free",
+            img: "/images/clothing6.jpeg",
+        },
+        {
+            type: "pants",
+            name: "pants1",
+            size: "M",
+            img: "/images/pants1.jpeg",
+        },
+        {
+            type: "pants",
+            name: "pants2",
+            size: "S",
+            img: "/images/pants2.jpeg",
+        },
+        {
+            type: "pants",
+            name: "pants3",
+            size: "S",
+            img: "/images/pants3.jpeg",
+        },
+        {
+            type: "pants",
+            name: "pants4",
+            size: "XS",
+            img: "/images/pants4.jpeg",
+        },
+    ],
+};
+
+export default storeItem;
--- pages/index.js
+++ pages/index.js
@@ -1,135 +1,22 @@
 import Image from "next/image";
+import storeItem from "../data/dummy";
 
 function StartPage() {
     return (
         <div className="flex">
             <ul className="list-main">
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing1.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">A</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing2.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">B</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing3.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">C</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing4.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">D</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing5.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">E</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing6.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">F</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/pants1.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">G</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/pants2.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">H</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/pants3.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">I</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/pants4.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">J</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing1.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">K</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing2.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">L</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing3.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">M</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing4.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">N</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing5.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">O</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing6.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">P</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/pants1.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">Q</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/pants2.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">R</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/pants3.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">S</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/pants4.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">T</div>
-                </li>
-                <li className="border-2 min-h-64 rounded-md">
-                    <div className="h-3/4">
-                        <Image className="rounded-t max-h-48" src="/images/clothing1.jpeg" alt="Clothing" width={800} height={500} />
-                    </div>
-                    <div className="p-2 h-1/4">U</div>
-                </li>
+                {storeItem.clothing.length > 0 ? (
+                    storeItem.clothing.map((item, idx) => (
+                        <li className="border-2 min-h-60 rounded-md" key={idx}>
+                            <div className="h-3/4">
+                                <Image className="rounded-t max-h-44" src={item.img} alt="Clothing" width={800} height={500} />
+                            </div>
+                            <div className="p-2 h-1/4">{item.name}</div>
+                        </li>
+                    ))
+                ) : (
+                    <div></div>
+                )}
             </ul>
             <div className="banner-main my-5 mr-5">
                 <div className="banner border-2 rounded-md">
