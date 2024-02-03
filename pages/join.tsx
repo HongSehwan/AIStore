07246386d2df658f33418dbdfd42eefc84ee7252
HongSehwan <gg9297@gmail.com>
2024-02-04 03:29:41 +0900

    회원가입 폼 수정/추가

--- pages/join.tsx
+++ pages/join.tsx
@@ -2,17 +2,33 @@ import React, { useEffect, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { userIdState, userPwState } from "@/store/recoil_atoms";
 import Image from "next/image";
+import Script from "next/script";
 import * as CryptoJS from "crypto-js";
 import { useRouter } from "next/router";
 
-interface JoinPageProps {
-    // Add any props if needed
+declare global {
+    interface Window {
+        daum: any;
+    }
+}
+
+interface IAddr {
+    address: string;
+    zonecode: string;
 }
 
 const JoinPage: React.FC = () => {
     const router = useRouter();
     const [id, setId] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [address, setAddress] = useState<string>(""); // 도로명주소
+    const [zipCode, setZipCode] = useState<string>(""); // 우편번호
+    const [addrDetail, setAddrDetail] = useState<string>(""); // 상세주소
+    const [nameText, setNameText] = useState<string>("");
+    const [firstNumber, setFirstNumber] = useState<string>("010");
+    const [middleNumber, setMiddleNumber] = useState<string>("");
+    const [lastNumber, setLastNumber] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
     const [storeId, setStoreId] = useRecoilState<string>(userIdState);
     const [storePW, setStorePW] = useRecoilState<string>(userPwState);
     const [confirmPassword, setConfirmPassword] = useState<string>("");
@@ -33,6 +49,22 @@ const JoinPage: React.FC = () => {
             setPassword(value);
         } else if (name === "confirmPassword") {
             setConfirmPassword(value);
+        } else if (name === "zipCode") {
+            setZipCode(value);
+        } else if (name === "address") {
+            setAddress(value);
+        } else if (name === "addressDetail") {
+            setAddrDetail(value);
+        } else if (name === "nameText") {
+            setNameText(value);
+        } else if (name === "firstNum") {
+            setFirstNumber(value);
+        } else if (name === "middleNum") {
+            setMiddleNumber(value);
+        } else if (name === "lastNum") {
+            setLastNumber(value);
+        } else if (name === "email") {
+            setEmail(value);
         }
     };
 
@@ -98,16 +130,28 @@ const JoinPage: React.FC = () => {
     }, [id, password, confirmPassword]);
 
     const handleJoin = () => {
-        if (id && password && confirmPassword) {
+        if (
+            nameText &&
+            id &&
+            password &&
+            confirmPassword &&
+            zipCode &&
+            address &&
+            addrDetail &&
+            firstNumber &&
+            middleNumber &&
+            lastNumber &&
+            email
+        ) {
             if (storeId === id) {
                 setJoin(false);
                 alert("중복된 아이디가 존재합니다.");
             } else if (!checkId(id)) {
                 setJoin(false);
-                alert("아이디 항목은 영문/숫자 포함 최대 15자입니다.");
+                alert("아이디는 영문, 숫자 포함 최대 15자입니다.");
             } else if (!checkPassword(password)) {
                 setJoin(false);
-                alert("비밀번호는 항목은 영문(대소문자)/숫자/특수기호 포함 최소 8자입니다.");
+                alert("영문(대소문자), 숫자, 특수문자를 혼합하여 입력해주세요.");
             } else {
                 const encrypted = CryptoJS.AES.encrypt(password, process.env.NEXT_PUBLIC_SECRET_KEY).toString();
                 setStoreId(id);
@@ -122,77 +166,219 @@ const JoinPage: React.FC = () => {
         }
     };
 
+    const checkIdHandler = () => {
+        alert("ID 중복 확인 완료");
+        /*
+            DB 생성 후 로직 완성하기
+        */
+    };
+
+    const onClickAddr = () => {
+        new window.daum.Postcode({
+            oncomplete: function (data: IAddr) {
+                setAddress(data.address);
+                setZipCode(data.zonecode);
+                setAddrDetail("");
+                document.getElementById("addressDetailInput")?.focus();
+            },
+        }).open();
+    };
+
     return (
-        <div className="flex justify-center text-center">
-            <div>
-                <div className="flex justify-center">
-                    <Image className="login-logo w-56 mt-5" src="/images/AIStoreLogo.webp" alt="LOGO" width={800} height={500} />
-                </div>
-                <div className="w-96 mt-6">
-                    {/*
+        <>
+            <Script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></Script>
+            <div className="flex justify-center text-center">
+                <div>
+                    <div className="flex justify-center">
+                        <Image className="login-logo w-56 mt-5" src="/images/AIStoreLogo.webp" alt="LOGO" width={800} height={500} />
+                    </div>
+                    <div>
+                        {/*
                         1. ID 중복검사 버튼
                         2. 주소
                         3. 핸드폰 번호(인증 번호 확인)
-                        4. 이메일
-                        5. 성별 선택
-                        6. 생년월일
-                        7. 이용약관 동의 및 14세 이상 동의
-                        8. 카카오/구글 소셜 로그인
+                        4. 이메일(이메일 형식 검사 정규표현식 추가)
+                        5. 이용약관 동의 및 14세 이상 동의
+                        6. 카카오/구글 소셜 로그인
                         추가
                     */}
-                    <input
-                        className="joinInput"
-                        style={
-                            idValidation
-                                ? { borderColor: "#b2bec3", borderStyle: "solid", borderWidth: "1px" }
-                                : { borderColor: "#74b9ff", borderStyle: "solid", borderWidth: "1px" }
-                        }
-                        ref={idInput}
-                        type="text"
-                        name="id"
-                        placeholder="아이디를 입력해주세요."
-                        value={id}
-                        required
-                        onChange={onChangeValue}
-                    />
-                    <input
-                        className="pwInput"
-                        style={
-                            pwValidation
-                                ? { borderColor: "#b2bec3", borderStyle: "solid", borderWidth: "1px" }
-                                : { borderColor: "#74b9ff", borderStyle: "solid", borderWidth: "1px" }
-                        }
-                        name="password"
-                        type="password"
-                        placeholder="비밀번호를 입력해주세요."
-                        value={password}
-                        required
-                        onChange={onChangeValue}
-                    />
-                    <input
-                        className="pwInput"
-                        style={
-                            confirmPwValidation
-                                ? { borderColor: "#b2bec3", borderStyle: "solid", borderWidth: "1px" }
-                                : { borderColor: "#74b9ff", borderStyle: "solid", borderWidth: "1px" }
-                        }
-                        name="confirmPassword"
-                        type="Password"
-                        placeholder="한번 더 비밀번호를 입력해주세요."
-                        value={confirmPassword}
-                        required
-                        onChange={onChangeValue}
-                    />
-                    <button
-                        className="joinBtn"
-                        style={btnValidation ? { backgroundColor: "#4bcffa" } : { backgroundColor: "#c7ecee" }}
-                        onClick={handleJoin}
-                    >
-                        <p className="joinBtnText">회원가입</p>
-                    </button>
+                        <div className="flex">
+                            <div className="w-96 mt-6">
+                                <div>
+                                    <p className="text-sm font-medium text-left ml-3.5 mt-3">이름</p>
+                                    <input
+                                        className="nameInput"
+                                        name="nameText"
+                                        type="name"
+                                        ref={idInput}
+                                        placeholder="이름를 입력해주세요."
+                                        value={nameText}
+                                        required
+                                        onChange={onChangeValue}
+                                    />
+                                </div>
+                                <div>
+                                    <p className="text-sm font-medium text-left ml-3.5 mt-3">아이디</p>
+                                    <div className="joinInputWrapper flex">
+                                        <input
+                                            className="joinInput"
+                                            style={
+                                                idValidation
+                                                    ? {
+                                                          borderColor: "#b2bec3",
+                                                          borderStyle: "solid",
+                                                          borderWidth: "1px",
+                                                          width: "300px",
+                                                      }
+                                                    : {
+                                                          borderColor: "#74b9ff",
+                                                          borderStyle: "solid",
+                                                          borderWidth: "1px",
+                                                          width: "300px",
+                                                      }
+                                            }
+                                            type="text"
+                                            name="id"
+                                            placeholder="아이디를 입력해주세요."
+                                            value={id}
+                                            required
+                                            onChange={onChangeValue}
+                                        />
+                                        <button className="duplicationCheck" onClick={checkIdHandler}>
+                                            중복확인
+                                        </button>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="text-sm font-medium text-left ml-3.5 mt-3">비밀번호</p>
+                                    <input
+                                        className="pwInput"
+                                        style={
+                                            pwValidation
+                                                ? { borderColor: "#b2bec3", borderStyle: "solid", borderWidth: "1px" }
+                                                : { borderColor: "#74b9ff", borderStyle: "solid", borderWidth: "1px" }
+                                        }
+                                        name="password"
+                                        type="password"
+                                        placeholder="영문(대소문자), 숫자, 특수기호 포함 최소 8자 이상"
+                                        value={password}
+                                        required
+                                        onChange={onChangeValue}
+                                    />
+                                </div>
+                                <div>
+                                    <p className="text-sm font-medium text-left ml-3.5 mt-3">비밀번호 확인</p>
+                                    <input
+                                        className="pwInput"
+                                        style={
+                                            confirmPwValidation
+                                                ? { borderColor: "#b2bec3", borderStyle: "solid", borderWidth: "1px" }
+                                                : { borderColor: "#74b9ff", borderStyle: "solid", borderWidth: "1px" }
+                                        }
+                                        name="confirmPassword"
+                                        type="Password"
+                                        placeholder="비밀번호를 확인해 주세요."
+                                        value={confirmPassword}
+                                        required
+                                        onChange={onChangeValue}
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-96 mt-6">
+                                <div>
+                                    <p className="text-sm font-medium text-left ml-3.5 mt-3">주소</p>
+                                    <div className="addressInputWrapper flex">
+                                        <input
+                                            id="zipCodeInput"
+                                            type="text"
+                                            name="zipCode"
+                                            placeholder="우편번호"
+                                            value={zipCode}
+                                            readOnly
+                                            required
+                                            onChange={onChangeValue}
+                                        />
+                                        <button className="addressSearch" onClick={onClickAddr}>
+                                            우편번호
+                                        </button>
+                                    </div>
+                                    <input
+                                        id="addressInput"
+                                        name="address"
+                                        type="text"
+                                        placeholder="도로명 주소를 입력해주세요."
+                                        value={address}
+                                        required
+                                        onChange={onChangeValue}
+                                    />
+                                    <input
+                                        id="addressDetailInput"
+                                        name="addressDetail"
+                                        type="text"
+                                        placeholder="상세 주소를 입력해주세요."
+                                        value={addrDetail}
+                                        required
+                                        onChange={onChangeValue}
+                                    />
+                                </div>
+                                <div>
+                                    <p className="text-sm font-medium text-left ml-3.5 mt-3">휴대전화</p>
+                                    <div className="phoneWrapper flex">
+                                        <select className="areaCode" name="firstNum" onChange={onChangeValue}>
+                                            <option value="010">010</option>
+                                            <option value="011">011</option>
+                                            <option value="016">016</option>
+                                            <option value="017">017</option>
+                                            <option value="018">018</option>
+                                            <option value="019">019</option>
+                                        </select>
+                                        <p className="enDash">–</p>
+                                        <input
+                                            className="phoneInput"
+                                            name="middleNum"
+                                            type="tel"
+                                            value={middleNumber}
+                                            required
+                                            onChange={onChangeValue}
+                                        />
+                                        <p className="enDash">–</p>
+                                        <input
+                                            className="phoneInput"
+                                            name="lastNum"
+                                            type="tel"
+                                            value={lastNumber}
+                                            required
+                                            onChange={onChangeValue}
+                                        />
+                                        <button className="telCheck">인증번호 받기</button>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="text-sm font-medium text-left ml-3.5 mt-3">이메일</p>
+                                    <input
+                                        className="emailInput"
+                                        name="email"
+                                        type="email"
+                                        value={email}
+                                        required
+                                        onChange={onChangeValue}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <button
+                                className="joinBtn"
+                                style={btnValidation ? { backgroundColor: "#4bcffa" } : { backgroundColor: "#c7ecee" }}
+                                onClick={handleJoin}
+                            >
+                                <p className="joinBtnText">회원가입</p>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -76,7 +76,6 @@ const LoginPage: React.FC = () => {
 
     const handleLogin = () => {
         if (id && password) {
-            console.log(storePW);
             if (storeId === "" || storePW === "") {
                 setLogoutAtom(() => false);
                 alert("회원정보가 없습니다. 회원가입 후 이용바랍니다.");
